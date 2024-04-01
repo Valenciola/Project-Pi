238f349dd73c99e0929ad1906a215154a2239304
Valenciola <vfilsaime2021@fau.edu>
2024-04-01 21:37:14 +0000

    Bare bones popup

---
+++ test-window/target.js
@@ -0,0 +1,21 @@
+//Testing stuff here
+let pop = document.getElementById("popup");
+
+let btn = document.getElementById("popbutton");
+btn.addEventListener("click", function() {
+	console.log("Click!");
+	pop.innerHTML = ("");
+});
+
+//Bare bones for a popup
+window.addEventListener("keydown", function(e) {
+	if (e.key == 'a') {
+		console.log("A");
+		pop.innerHTML = ("<button id=\"popbutton\">X</button><p>Hello</p><p>Some extra text to fill area</p>");
+	}
+	let btn = document.getElementById("popbutton");
+	btn.addEventListener("click", function() {
+	console.log("Click!");
+	pop.innerHTML = ("");
+	});
+});
