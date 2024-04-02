aae27ed9427fd7dcd3212aeb7e52648148b3b31b
Valenciola <vfilsaime2021@fau.edu>
2024-04-02 01:43:47 +0000

    Some styling fundamentals; removes and reappears pretty easily but stuffing images in it is gonna be an entirely new ball game but you know nothing I can't handle

--- test-window/target.js
+++ test-window/target.js
@@ -6,17 +6,20 @@ let btn = document.getElementById("popbutton");
 btn.addEventListener("click", function() {
 	console.log("Click!");
 	pop.innerHTML = ("");
+	pop.style.display = "none";
 });
 
 //Bare bones for a popup
 window.addEventListener("keydown", function(e) {
 	if (e.key == 'a') {
 		console.log("A");
-		pop.innerHTML = ("<button id=\"popbutton\">X</button><p>Hello</p><p>Some extra text to fill area</p>");
+		pop.innerHTML = ("<button id=\"popbutton\">X</button><p>Hello</p><p>Some extra text to fill area</p><p>Is there an expansion if I type more than I already have?</p>");
+		pop.style.display = "initial";
 	}
 	let btn = document.getElementById("popbutton");
 	btn.addEventListener("click", function() {
-	console.log("Click!");
-	pop.innerHTML = ("");
+		console.log("Click!");
+		pop.innerHTML = ("");
+		pop.style.display = "none";
 	});
 });
