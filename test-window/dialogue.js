e45707247c0b017acca26b1eeeed436437fa56eb
Valenciola <valeriez3012@gmail.com>
2024-07-10 19:20:17 -0400

    Various

--- test-window/dialogue.js
+++ test-window/dialogue.js
@@ -1,19 +1,54 @@
+// Dialogue box initial stuff
+let dio = document.getElementById("dio");
+window.addEventListener("keydown", function(e) {
+	if (e.key == 'd' || e.key == 'b') {
+		//console.log("Dia!");
+		dio.style.display = "initial";
+	}
+	else {
+		dio.style.display = "none";
+	}
+});
+
 // Working text engine (with useful parameter input)
 let i = 0;
+let marker = false;
+
 const testRunText = function(text) {
-	while (i <= text.length) {
-		const printed = text.substring(0, i);
+    //Function that runs the code to print text
+    marker = false;
+	if (i <= text.length) {
+		let printed = text.substring(0, i);
+        dio.textContent = printed;
 		console.log(printed);
 		i++;
 		// var helpTest = console.log (i === textTest.length + 1);
 	}
-};
-const letsGoPrint = function(dialogue) {
-	const printText = window.setInterval(testRunText, 100, dialogue);
-	if (helpTest === true) {
-		window.clearInterval(printText);
-		console.log('Quit dialogue');
-	}
+    else {
+        marker = true;
+    }
 };
 
-letsGoPrint('test');
+// function keeps alternating between all three lines for some reason; got to find a fix
+let lines = ["First line", "second line", "third line"];
+
+async function runDialogue(text) {
+
+    for (let lines of text) {
+        i = 0;
+        let runner = window.setInterval(testRunText, 100, lines);
+
+        await new Promise((resolve) => {
+            document.addEventListener('keydown', (event) => {
+                if (event.key === 'b') {
+                    resolve();
+                }
+            });
+        });
+    }
+    // console.log("End");
+}
+
+// Call the function to start waiting
+
+runDialogue(lines);
